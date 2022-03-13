0a7e10ba39c7e090017bdff90e5a5baa43cc0bec
joey tierney <joey.tierney@marinosoftware.com>
2022-03-13 17:41:03 +0000

    feat: added which DLC survivors are in to output

--- src/App.vue
+++ src/App.vue
@@ -4,8 +4,8 @@
   </div>
   <router-view />
   <iframe
-    width="1"
-    height="1"
+    width="0"
+    height="0"
     src="https://www.youtube.com/embed/nc2yOLMCOhs?autoplay=1&loop=1&rel=0&showinfo=0&color=white&iv_load_policy=3&playlist=nc2yOLMCOhs"
     title="YouTube video player"
     frameborder="0"
