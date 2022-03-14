f5f67b997862c88c91f7d04046e0484850801e9a
joey tierney <joey.tierney@marinosoftware.com>
2022-03-14 14:15:07 +0000

    feat: started adding survivor perks

--- src/views/dropdown/survivor-perks.vue
+++ src/views/dropdown/survivor-perks.vue
@@ -1,3 +1,144 @@
 <template>
   <h1>Survivor Perks</h1>
+  <div class="perks" v-if="visible">
+    <div class="perk1">
+      <img :src="selectedSurvPerk.image" alt="" width="200" />
+      <h1 class="perkName">{{ selectedSurvPerk.name }}</h1>
+      <h3 class="perkTeach">{{ selectedSurvPerk.teachable }}</h3>
+    </div>
+    <div class="perk2">
+      <img :src="selectedSurvPerk2.image" alt="" width="200" />
+      <h1 class="perkName">{{ selectedSurvPerk2.name }}</h1>
+      <h3 class="perkTeach">{{ selectedSurvPerk2.teachable }}</h3>
+    </div>
+    <div class="perk3">
+      <img :src="selectedSurvPerk3.image" alt="" width="200" />
+      <h1 class="perkName">{{ selectedSurvPerk3.name }}</h1>
+      <h3 class="perkTeach">{{ selectedSurvPerk3.teachable }}</h3>
+    </div>
+    <div class="perk4">
+      <img :src="selectedSurvPerk4.image" alt="" width="200" />
+      <h1 class="perkName">{{ selectedSurvPerk4.name }}</h1>
+      <h3 class="perkTeach">{{ selectedSurvPerk4.teachable }}</h3>
+    </div>
+  </div>
+  <img
+    src="/assets/icons/perk-empty-icon.png"
+    alt="Random Survivor Perk Placeholder"
+    class="emptyPerk"
+    id="emptyPerk"
+    width="200"
+  />
+  <p id="helperText">Click the Perk icon below!</p>
+  <br />
+  <p>
+    <img
+      src="/assets/icons/perk-icon.png"
+      alt="Click here for a random set of perks!"
+      @click="rndPerk"
+      width="90"
+      height="90"
+      class="perkButton"
+    />
+  </p>
 </template>
+
+<script>
+import { survivorPerks } from "/utils/SurvivorPerks.js";
+export default {
+  data() {
+    return {
+      survivorPerks: survivorPerks,
+      selectedSurvPerk: "",
+      selectedSurvPerk2: "",
+      selectedSurvPerk3: "",
+      selectedSurvPerk4: "",
+      visible: false,
+    };
+  },
+  methods: {
+    rndPerk() {
+      const idx = Math.floor(Math.random() * this.survivorPerks.length);
+      const idx2 = Math.floor(Math.random() * this.survivorPerks.length);
+      const idx3 = Math.floor(Math.random() * this.survivorPerks.length);
+      const idx4 = Math.floor(Math.random() * this.survivorPerks.length);
+      this.selectedSurvPerk = this.survivorPerks[idx];
+      this.selectedSurvPerk2 = this.survivorPerks[idx2];
+      this.selectedSurvPerk3 = this.survivorPerks[idx3];
+      this.selectedSurvPerk4 = this.survivorPerks[idx4];
+      document.getElementById("emptyPerk").remove();
+      document.getElementById("helperText").remove();
+      this.visible = true;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 20px;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 50px;
+  margin: 0 0 15px 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #fff;
+}
+
+h3 {
+  font-size: 14px;
+  font-weight: bold;
+  font-style: normal;
+  line-height: 14px;
+  margin: 0 0 10px 0;
+  text-align: center;
+  text-transform: uppercase;
+  color: #aa0101;
+}
+
+img {
+  color: white;
+}
+
+.questionMark {
+  height: 90px;
+  width: 90px;
+}
+
+.survivors {
+  margin-top: 1.2rem;
+}
+
+.perkButton {
+  cursor: pointer;
+}
+
+.perkButton:active {
+  transform: translateY(4px);
+}
+
+.perk1 {
+  float: left;
+  width: 20%;
+  height: 280px;
+}
+
+.perk2 {
+  float: left;
+  width: 20%;
+  height: 280px;
+}
+
+.perk3 {
+  float: right;
+  width: 20%;
+  height: 280px;
+}
+
+.perk4 {
+  float: right;
+  width: 20%;
+  height: 280px;
+}
+</style>
---
+++ utils/SurvivorPerks.js
@@ -0,0 +1,248 @@
+export const survivorPerks = [
+    {
+        name: "Ace in the Hole",
+        image:
+          "../../assets/perks/survivor-perks/ace-in-the-hole.png",
+        teachable: "Ace Visconti Teachable Perk",
+    },
+    {
+        name: "Adrenaline",
+        image:
+          "../../assets/perks/survivor-perks/adrenaline.png",
+        teachable: "Meg Thomas Teachable Perk",
+    },
+    {
+        name: "After Care",
+        image:
+          "../../assets/perks/survivor-perks/after-care.png",
+        teachable: "Jeff Johansen Teachable Perk",
+    },
+    {
+        name: "Alert",
+        image:
+          "../../assets/perks/survivor-perks/alert.png",
+        teachable: "Feng Min Teachable Perk",
+    },
+    {
+        name: "Any Means Necessary",
+        image:
+          "../../assets/perks/survivor-perks/any-means-necessary.png",
+        teachable: "Yui Kimura Teachable Perk",
+    },
+    {
+      name: "Appraisal",
+      image:
+        "../../assets/perks/survivor-perks/appraisal.png",
+      teachable: "Élodie Rakoto Teachable Perk",
+    },
+    {
+      name: "Autodidact",
+      image:
+        "../../assets/perks/survivor-perks/autodidact.png",
+      teachable: "Adam Francis Teachable Perk",
+    },
+    {
+      name: "Babysitter",
+      image:
+        "../../assets/perks/survivor-perks/babysitter.png",
+      teachable: "Steve Harrington Teachable Perk",
+    },
+    {
+      name: "Balanced Landing",
+      image:
+        "../../assets/perks/survivor-perks/balanced-landing.png",
+      teachable: "Nea Karlsson Teachable Perk",
+    },
+    {
+      name: "Better Together",
+      image:
+        "../../assets/perks/survivor-perks/better-together.png",
+      teachable: "Nancy Wheeler Teachable Perk",
+    },
+    {
+      name: "Bite the Bullet",
+      image:
+        "../../assets/perks/survivor-perks/bite-the-bullet.png",
+      teachable: "Leon S. Kennedy Teachable Perk",
+    },
+    {
+      name: "Blast Mine",
+      image:
+        "../../assets/perks/survivor-perks/blast-mine.png",
+      teachable: "Jill Valentine Teachable Perk",
+    },
+    {
+      name: "Blood Pact",
+      image:
+        "../../assets/perks/survivor-perks/blood-pact.png",
+      teachable: "Cheryl Mason Teachable Perk",
+    },
+    {
+      name: "Boil Over",
+      image:
+        "../../assets/perks/survivor-perks/boil-over.png",
+      teachable: "Kate Denson Teachable Perk",
+    },
+    {
+      name: "Bond",
+      image:
+        "../../assets/perks/survivor-perks/bond.png",
+      teachable: "Dwight Fairfield Teachable Perk",
+    },
+    {
+      name: "Boon: Circle of Healing",
+      image:
+        "../../assets/perks/survivor-perks/boon-circle-of-healing.png",
+      teachable: "Mikaela Reid Teachable Perk",
+    },
+    {
+      name: "Boon: Dark Theory",
+      image:
+        "../../assets/perks/survivor-perks/boon-dark-theory.png",
+      teachable: "Yoichi Asakawa Teachable Perk",
+    },
+    {
+      name: "Boon: Exponential",
+      image:
+        "../../assets/perks/survivor-perks/boon-exponential.png",
+      teachable: "Jonah Vasquez Teachable Perk",
+    },
+    {
+      name: "Boon: Shadow Step",
+      image:
+        "../../assets/perks/survivor-perks/boon-shadow-step.png",
+      teachable: "Mikaela Reid Teachable Perk",
+    },
+    {
+      name: "Borrowed Time",
+      image:
+        "../../assets/perks/survivor-perks/borrowed-time.png",
+      teachable: 'William "Bill" Overbeck Teachable Perk',
+    },
+    {
+      name: "Botany Knowledge",
+      image:
+        "../../assets/perks/survivor-perks/botany-knowledge.png",
+      teachable: "Claudette Morel Teachable Perk",
+    },
+    {
+      name: "Breakdown",
+      image:
+        "../../assets/perks/survivor-perks/breakdown.png",
+      teachable: "Jeff Johansen Teachable Perk",
+    },
+    {
+      name: "Breakout",
+      image:
+        "../../assets/perks/survivor-perks/breakout.png",
+      teachable: "Yui Kimura Teachable Perk",
+    },
+    {
+      name: "Buckle Up",
+      image:
+        "../../assets/perks/survivor-perks/buckle-up.png",
+      teachable: "Ashley J. Williams Teachable Perk",
+    },
+    {
+      name: "Built to Last",
+      image:
+        "../../assets/perks/survivor-perks/built-to-last.png",
+      teachable: "Felix Richter Teachable Perk",
+    },
+    {
+      name: "Calm Spirit",
+      image:
+        "../../assets/perks/survivor-perks/calm-spirit.png",
+      teachable: "Jake Park Teachable Perk",
+    },
+    {
+      name: "Camaraderie",
+      image:
+        "../../assets/perks/survivor-perks/camaraderie.png",
+      teachable: "Steve Harrington Teachable Perk",
+    },
+    {
+      name: "Clairvoyance",
+      image:
+        "../../assets/perks/survivor-perks/clairvoyance.png",
+      teachable: "Mikaela Reid Teachable Perk",
+    },
+    {
+      name: "Corrective Action",
+      image:
+        "../../assets/perks/survivor-perks/corrective-action.png",
+      teachable: "Jonah Vasquez Teachable Perk",
+    },
+    {
+      name: "Counterforce",
+      image:
+        "../../assets/perks/survivor-perks/counter-force.png",
+      teachable: "Jill Valentine Teachable Perk",
+    },
+    {
+      name: "Dance With Me",
+      image:
+        "../../assets/perks/survivor-perks/dance-with-me.png",
+      teachable: "Kate Denson Teachable Perk",
+    },
+    {
+      name: "Dark Sense",
+      image:
+        "../../assets/perks/survivor-perks/dark-sense.png",
+      teachable: "All Characters",
+    },
+    {
+      name: "Dead Hard",
+      image:
+        "../../assets/perks/survivor-perks/dead-hard.png",
+      teachable: "David King Teachable Perk",
+    },
+    {
+      name: "Deception",
+      image:
+        "../../assets/perks/survivor-perks/deception.png",
+      teachable: "Élodie Rakoto Teachable Perk",
+    },
+    {
+      name: "Decisive Strike",
+      image:
+        "../../assets/perks/survivor-perks/decisive-strike.png",
+      teachable: "Laurie Strode Teachable Perk",
+    },
+    {
+      name: "Déjà Vu",
+      image:
+        "../../assets/perks/survivor-perks/deja-vu.png",
+      teachable: "All Characters",
+    },
+    {
+      name: "Deliverance",
+      image:
+        "../../assets/perks/survivor-perks/deliverance.png",
+      teachable: "Adam Francis Teachable Perk",
+    },
+    {
+      name: "Desperate Measures",
+      image:
+        "../../assets/perks/survivor-perks/desperate-measures.png",
+      teachable: "Felix Richter Teachable Perk",
+    },
+    {
+      name: "Detectives Hunch",
+      image:
+        "../../assets/perks/survivor-perks/detectives-hunch.png",
+      teachable: "Detective David Tapp Teachable Perk",
+    },
+    {
+      name: "Distortion",
+      image:
+        "../../assets/perks/survivor-perks/distortion.png",
+      teachable: "Jeff Johansen Teachable Perk",
+    },
+    {
+      name: "Diversion",
+      image:
+        "../../assets/perks/survivor-perks/diversion.png",
+      teachable: "Adam Francis Teachable Perk",
+    },
+]
