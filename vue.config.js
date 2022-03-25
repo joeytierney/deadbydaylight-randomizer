9aa9dbe44672c0ae0c0471827d78ebcb7510cd50
joey tierney <joey.tierney@marinosoftware.com>
2022-03-25 10:13:36 +0000

    chore: changed config file

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/deadbydaylight-randomizer/" : "/",
+  publicPath: process.env.NODE_ENV === "production" ? "/joeytierney.github.io/" : "/",
 };
