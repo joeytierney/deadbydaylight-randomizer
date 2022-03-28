929301f5aa0a82c6705333e78994cf21a864687b
joey tierney <joey.tierney@marinosoftware.com>
2022-03-28 09:59:09 +0100

    chore: attempted fix v2

--- src/router/index.js
+++ src/router/index.js
@@ -1,59 +1,21 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
-import Survivors from "../views/Survivors.vue";
-import Killers from "../views/Killers.vue";
-import Perks from "../views/Perks.vue";
-import SurvivorPerks from "../views/dropdown/survivor-perks.vue";
-import KillerPerks from "../views/dropdown/killer-perks.vue";
-import PrivacyPolicy from "../views/PrivacyPolicy.vue";
-import Error404 from "../views/404.vue";
 
 const routes = [
   {
     path: "/",
+    redirect: { name: 'Home' }
+  },
+  {
+    path: "/deadbydaylight-randomizer/",
     name: "Home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    component: About,
-  },
-  {
-    path: "/survivors",
-    name: "Survivors",
-    component: Survivors,
-  },
-  {
-    path: "/killers",
-    name: "Killers",
-    component: Killers,
-  },
-  {
-    path: "/perks",
-    name: "Perks",
-    component: Perks,
-  },
-  {
-    path: "/perks/survivor-perks",
-    name: "SurvivorPerks",
-    component: SurvivorPerks,
-  },
-  {
-    path: "/perks/killer-perks",
-    name: "KillerPerks",
-    component: KillerPerks,
-  },
-  {
-    path: "/:catchAll(.*)",
-    name: "Error404",
-    component: Error404,
-  },
-  {
-    path: "/privacy-policy",
-    name: "PrivacyPolicy",
-    component: PrivacyPolicy,
+    path: "/deadbydaylight-randomizer/:queryParams(.*)",
+    name: "Result",
+    component: Result,
+    props: true,
   },
 ];
 
